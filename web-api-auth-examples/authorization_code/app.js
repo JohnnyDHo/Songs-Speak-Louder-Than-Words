06c2d275e5676677d72b41cfa68af3940bb79a4a
JohnnyDHo <johnnyyyyho1999@gmail.com>
2020-01-04 21:12:43 -0500

    Fresh start on front-end

--- web-api-auth-examples/authorization_code/app.js
+++ web-api-auth-examples/authorization_code/app.js
@@ -13,8 +13,8 @@ var cors = require('cors');
 var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
 
-var client_id = '6502b814f9e14d3fa6da052ac373f2bc'; // Your client id
-var client_secret = '0523031a9e8a42e5a0fbc899bc91e568'; // Your secret
+var client_id = ''; // Your client id
+var client_secret = ''; // Your secret
 var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
 
 /**
